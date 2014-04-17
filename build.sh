276b2b14c06574e6f11ff49ec232865e215a31d7
Stas Alekseev <stas.alekseev@gmail.com>
2014-04-17 16:53:17 -0400

    Adding init scripts.

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-fpm -s dir -t rpm -n athena-flume-ng --license "ASL 2.0" -v 1.4.0 --iteration cdh4.6.0.ath usr
+fpm -s dir -t rpm -n athena-flume-ng --license "ASL 2.0" -v 1.4.0 --iteration cdh4.6.0.ath --epoch 1 etc usr
